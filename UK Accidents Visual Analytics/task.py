42bbfb617d62d97f3d28a4b41be4e9a2df4e84c8
Ridampreet Singh Jaggi <37906819+ridampreet@users.noreply.github.com>
2022-01-02 19:38:52 -0400

    Update task.py

--- UK Accidents Visual Analytics/task.py
+++ UK Accidents Visual Analytics/task.py
@@ -29,50 +29,7 @@ import dash_bootstrap_components as dbc
 
 
 def task():
-    """
-    For the last assignment, there is only one task, which will use your knowledge from all previous assignments.
-    If your methods of a1, a2 and a3 are well implemented, a4 will be fairly simple, so reuse the methods if possible for your own
-    benefit! If you prefer, you can reimplement any logic you with in the assignment4 folder.
 
-    For this task, feel free to create new files, modules or methods in this file as you see fit. Our test will be done by calling this
-    task() method, and we expect to receive the dash app back (similar to a3) and we will run it. No other method will be called by us, so
-    make sure your code is running as expected. We will basically run this code: `task().run_server(debug=True)`
-
-    For this task, you will build a dash app where we can perform a simple form of interactivity on it. We will use the accidents.csv
-    dataset. This accidents.csv dataset has information about traffic accidents in the UK, and is available to you now.
-    You will show the most accident prone areas in the UK on a map, so the regular value shown in the map should let us know the number of accidents
-    that occurred in a specific area of the map (which should include the accident's severity as well as a weight factor). That said, the purpose of
-    this map is to be used by the police to identify how they can do things better.
-
-    **This is raw data, so preprocess the data as per requirement. Drop columns that you feel are unnecessary for the analysis or clustering.
-    Don't forget to add comments about why you did what you did**
-
-
-    ##############################################################
-    # Your task will be to Implement all the below functionalities
-    ##############################################################
-
-    1. (40pts) Implement a map with the GPS points for the accidents per month. Have a slider(#slider1) that can be used to filter accident data for the month I need.
-        You are free to choose a map style, but I suggest using a scatter plot map.
-
-    2. (10pts) Implement a dropdown to select few of the numerical columns in the dataset that can be used meaningfully to represent the size of the GPS points.
-        By default the size of the GPS point on map should be based on the value of "accident_severity" column.
-
-    3. (30pts) You have to Cluster the points now. Be sure to have a text somewhere on the webpage that says what clustering algorithm you are using (e.g. KMeans, dbscan, etc).
-        For clustering, you should run a clustering method over the dataset (it should run fairly fast for quick iteration, so make sure to use a simple clustering procedure)
-        **COLOR** the GPS points based on the clusetring label returned from the algorithm.
-
-    4. (10pts) Have a button(#run_clustering) to run or rerun the clustering algorithm over the filtered dataset (filtered using #slider1 to select months).
-
-    5. (10pts) At least one parameter of the clustering algorithm should be made available for us to tinker with as a button/slider/dropdown.
-        When I change it and click #run_clustering button, your code should rerun the clustering algorithm.
-        example: change the number of clusters in kmeans or eps value in dbscan.
-
-        Please note: distance values should be in meters, for example dbscan uses eps as a parameter. This value should be read in mts from users and converted appropriately to be used in clustering,
-        so input_eps=100 should mean algorithm uses 100mts circle for finding core and non-core points.
-
-    The total points is 100pts
-    """
 
     # Map used,citation:https: // plotly.com / python / mapbox - layers /
     df3 = pd.read_csv('accidents.csv')
